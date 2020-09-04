040623ee5f2a2617aca018cda9ddde182f7f3bab
balogunjamiu49@gmail.com <balogunjamiu49@gmail.com>
2020-09-04 04:32:29 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+TweenMax.from(".content", 0.2, {
+    delay: 0.5,
+    x:"100%",
+    ease: Expo.easeInOut
+});
+TweenMax.from(".test", 0.5, {
+    delay: 1,
+    x:"-200%",
+    ease: Expo.easeInOut
+});
